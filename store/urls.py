8016f94e11880992d92ddb7ad3cce798ac89c3d2
Saket6198 <137860573+Saket6198@users.noreply.github.com>
2023-10-02 21:36:03 +0530

    Update urls.py
    removed unused urls and added two new urls for the add to cart functionality

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
-    path('home/', views.home, name="home"),
+    path('update_item/', views.updateItem, name="update_Item"),
+    path('process_order/',views.processOrder, name="process_order")
 ]
