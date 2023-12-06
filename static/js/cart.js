ce07f98b06f13b606aa3b7d0c4cf888b56ba452a
Saket6198 <137860573+Saket6198@users.noreply.github.com>
2023-12-06 13:07:10 +0530

    Update cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,17 +1,42 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+function initUpdateBtns() {
+    var updateBtns = document.getElementsByClassName('update-cart')
 
+    for (i = 0; i < updateBtns.length; i++) {
+        updateBtns[i].addEventListener('click', function(){
+            var productId = this.dataset.product
+            var action = this.dataset.action
+            console.log('productId:', productId, 'Action:', action)
+            console.log('USER:', user)
+            if(user == 'AnonymousUser'){
+                console.log('Not logged in')
+            }else{
+                    updateUserOrder(productId, action)
+            }
+        })
+    }
+}
 
-for(var i = 0; i < updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function(){
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('productId:', productId, 'action:', action )
+document.addEventListener('DOMContentLoaded', initUpdateBtns)
+function updateUserOrder(productId, action){
+     console.log('User is logged in, sending data...')
 
-        console.log('USER:', user)
-        if(user == 'AnonymousUser'){
-            console.log('Not logged in')
-        }else{
-            console.log('User is logged in, sending data...')
-        }
-    })
+     var url = '/update_item/'
+
+     fetch(url, {
+        method: 'POST',
+        headers:{
+        'Content-Type': 'application/json',
+        'X-CSRFTOKEN': csrftoken,
+        },
+        body:JSON.stringify({'productId': productId, 'action': action})
+     })
+
+     .then((response) =>{
+        return response.json()
+     })
+
+     .then((data) =>{
+        console.log('data:', data)
+        location.reload()
+     })
 }
